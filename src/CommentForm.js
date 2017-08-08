bf20b474a4a4dd5055b455d35dd260e9ca74b07b
NrupM <maryenorthrup@gmail.com>
2017-08-07 17:48:52 -0700

    built CommentForm component

---
+++ src/CommentForm.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import style from './style';
+
+class CommentForm extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      author: '',
+      text: ''
+    }
+  }
+  handleAuthorChange(e){
+    this.setState({
+      author: e.target.value
+    });
+  }
+  handleTextChange(e){
+    this.setState({
+      text: e.target.value
+    })
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    console.log(`${this.state.author} said "${this.state.text}"`);
+  }
+  render(){
+    return(
+      <div>
+        <h2>Create Comment Here!</h2>
+        <form style={ style.commentForm } onSubmit={ e => this.handleSubmit(e) }>
+        <input
+          type='text'
+          placeholder='Your name...'
+          style={ style.commentFormAuthor }
+          value={ this.state.author }
+          onChange={e => this.handleAuthorChange(e)} />
+        <input
+          type='text'
+          placeholder='Say something...'
+          style={ style.commentFormText }
+          value={ this.state.text }
+          />
+      </div>
+    )
+  }
+}
+
+export default CommentForm;
