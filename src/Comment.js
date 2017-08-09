c4642bb4a836dcda5fe6ff1841e24c7f06f2b265
NrupM <maryenorthrup@gmail.com>
2017-08-09 04:02:10 -0700

    update added to Comment component

--- src/Comment.js
+++ src/Comment.js
@@ -2,13 +2,85 @@ import React, { Component } from 'react';
 import style from './style';
 
 class Comment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tobeUpdated: false,
+      author: '',
+      text: ''
+    };
+    //bind
+  }
+  updateComment(e) {
+    e.preventDefault();
+    this.setState({ toBeUpdate: !this.state.toBeUpdated });
+  }
+  handleCommentUpdate(e) {
+    e.preventDefault();
+    let id = this.props.uniqueID;
+    //if author or text changed, set it. otherwise, leave null so POST request will ignore it
+    let author = this.state.author ? this.state.author : null;
+    let text = this.state.text ? this.state.text : null;
+    let comment = { author: author, text: text };
+    this.props.onCommentUpdate(id, comment);
+    this.setState({
+      toBeUpdated: !this.state.toBeUpdated,
+      author: '',
+      text: ''
+    });
+  }
+  deleteComment(e) {
+    e.preventDefault();
+    let id = this.props.uniqueID;
+    this.props.onCommentDelete(id);
+    console.log('oops deleted');
+  }
+  handleTextChange(e) {
+    this.setState({ text: e.target.value });
+  }
+  handleAuthorChange(e) {
+    this.setState({ author: e.target.value });
+  }
   render() {
-      return (
-          <div style={ style.comment }>
-            <h3>{this.props.author}</h3>
-            <span>{this.props.text}</span>
-          </div>
-      )
+    return (
+      <div style={style.comment}>
+        <h3>
+          {this.props.author}
+        </h3>
+        <span>
+          {this.props.text}
+        </span>
+        <a style={style.updateLink} href="#" onClick={this.updateComment}>
+          update
+        </a>
+        <a style={style.deleteLink} href="#" onClick={this.deleteComment}>
+          delete
+        </a>
+        {this.state.toBeUpdated
+          ? <form onSubmit={this.handleCommentUpdate}>
+              <input
+                type="text"
+                placeholder="Update name..."
+                style={style.commentFormAuthor}
+                value={this.state.author}
+                onChange={this.handleAuthorChange}
+              />
+              <input
+                type="text"
+                placeholder="Update your comment..."
+                style={style.commentFormText}
+                value={this.state.text}
+                onChange={this.handleTextChange}
+              />
+              <input
+                type="submit"
+                style={style.commentFormPost}
+                value="Update"
+              />
+            </form>
+          : null}
+      </div>
+    );
   }
 }
 
